50fb966997a7e747a481a626f1b5b33f66573c5c
e1q24062 <e1q24062@oit.ac.jp>
2024-10-15 17:45:29 +0900

    永田　Operationをいじった

--- Assets/Scripts/NMain/Operation.cs
+++ Assets/Scripts/NMain/Operation.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class Operation : MonoBehaviour
 {
+    public GameObject sumaho;
+    public int a = 0;
+    private bool see=false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sumaho.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Q))
-        { 
-            
+        if (Input.GetKeyDown(KeyCode.Q)&&a==0)
+        {
+            see = !see;
+            sumaho.SetActive(see);
         }
     }
     
